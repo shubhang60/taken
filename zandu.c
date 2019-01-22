331dd32ab5efd10c5a36c7244ae698bc6669beca
Wable Sanket <sanketwable123@gmail.com>
2019-01-22 11:00:42 +0530

    add zandu.c to initial commit

--- zandu.c
+++ zandu.c
@@ -3,6 +3,8 @@ int main()
 {
 printf("no.1 ayurvedic remedy for strong headache\n");
 printf("Zandu Balm Ultra Power\n");
+int a;
+printf("You have printed %d",a);
 printf("i am sanket and i have changed somthing in thi repository\n");
 return(0);
 }
