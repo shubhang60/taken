dce3962b00a32e736ceab2050e741a56d157182b
Wable Sanket <sanketwable123@gmail.com>
2019-01-22 10:40:54 +0530

    my snapshot

--- zandu.c
+++ zandu.c
@@ -3,5 +3,6 @@ int main()
 {
 printf("no.1 ayurvedic remedy for strong headache\n");
 printf("Zandu Balm Ultra Power\n");
+printf("i am sanket and i have changed somthing in thi repository\n");
 return(0);
 }
