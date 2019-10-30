50c90c6a1649def34eb3716d62507dba46eb57ff
Shubhang <56973512+shubhang60@users.noreply.github.com>
2019-10-30 14:02:28 +0530

    Update zandu.c

--- zandu.c
+++ zandu.c
@@ -3,7 +3,8 @@ int main()
 {
 printf("no.1 ayurvedic remedy for strong headache\n");
 printf("Zandu Balm Ultra Power\n");
-int a;
+int a,b
+;
 printf("You have printed %d",a);
 printf("i am sanket and i have changed many thing in this repository\n");
 printf("I'm Avinal and I corrected the spelling of 'this'");
